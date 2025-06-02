1fa550b0f59f6d573be8141e522ec06646a052fc
Mitt10 <69619899+Mitt10@users.noreply.github.com>
2025-06-02 16:42:52 -0700

    Update shift_register_0.v

--- Adder/Register/Shift Register/shift_register_0.v
+++ Adder/Register/Shift Register/shift_register_0.v
@@ -4,7 +4,7 @@
 // Concatenation-based template
 // File: shift_registers_0.v
 //This code shifts the bit from left to right, left being LSB and at every clock edge, if clken = 1, then MSB is shifted out and becomes output SO
-
+`timescale 1ns / 1ps
 module shift_register_0 (
   input clk, clken, SI,
   output SO
