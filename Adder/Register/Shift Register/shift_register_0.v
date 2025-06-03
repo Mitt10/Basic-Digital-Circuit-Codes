674f8c45e1816ba6444c29ad4e3413fd8161af62
Mitt10 <69619899+Mitt10@users.noreply.github.com>
2025-06-02 17:35:51 -0700

    Update shift_register_0.v

--- Adder/Register/Shift Register/shift_register_0.v
+++ Adder/Register/Shift Register/shift_register_0.v
@@ -6,16 +6,21 @@
 //This code shifts the bit from left to right, left being LSB and at every clock edge, if clken = 1, then MSB is shifted out and becomes output SO
 `timescale 1ns / 1ps
 module shift_register_0 (
-  input clk, clken, SI,
+  parameter WIDTH = 32;)
+  (
+  input clk, rst, clken, SI,
   output SO
 );
-  parameter WIDTH = 32;
   reg [WIDTH-1:0] shreg;
 
   always @(posedge clk)
     begin
-      if (clken)
-        shreg = {shreg[WIDTH-2:0], SI};
+      if (rst) begin
+        shreg <= 0;
+      end
+      else if (clken) begin
+        shreg <= {shreg[WIDTH-2:0], SI};
+      end    
     end
 
   assign SO = shreg[WIDTH-1];
