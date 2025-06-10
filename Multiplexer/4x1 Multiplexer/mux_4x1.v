1c96fcd1515f6b0c5016edcc1e407ddb8eaca4ab
Mitt10 <69619899+Mitt10@users.noreply.github.com>
2025-06-09 20:05:22 -0700

    Update mux_4x1.v

--- Multiplexer/4x1 Multiplexer/mux_4x1.v
+++ Multiplexer/4x1 Multiplexer/mux_4x1.v
@@ -3,8 +3,8 @@
 
 module mux_4x1(
 	input [1:0] sel,
-	input [3:0]i0,i1,i2,i3,
-	output reg[3:0] y
+	input i0,i1,i2,i3,
+	output reg y
 );
 
 always @(*)
@@ -18,5 +18,10 @@ begin
 		default: y=i0;
 	endcase
 end
+//Alternative approach for 4x1 Multiplexer
+//	assign y = (sel == 2'b00) ? i0 :
+//			   (sel == 2'b01) ? i1 :
+//			   (sel == 2'b10) ? i2 :
+//			   (sel == 2'b11) ? i3 : i0;
 
 endmodule
