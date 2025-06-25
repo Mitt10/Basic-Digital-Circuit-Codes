5bec15ce49d304855614a88897586a76a24f32cf
Mitt10 <mittparmar10@gmail.com>
2025-06-24 19:51:00 -0700

    Code changes

--- Multiplier/Array Multiplier/array_multipler.v
+++ Multiplier/Array Multiplier/array_multipler.v
@@ -1,46 +1,65 @@
-module half_adder(input a, b, output s0, c0);
-  assign s0 = a ^ b;
-  assign c0 = a & b;
-endmodule
+`timescale 1ns/1ps
 
-module full_adder(
-    input in1, in2, Cin,
-    output Cout, sum
-    );
+//module full_adder1(
+//    input in1, in2, Cin,
+//    output Cout, sum
+//    );
     
-assign sum= in1 ^ in2 ^ Cin;
-assign Cout = ((in1 ^ in2) & Cin) | (in1 & in2); 
-endmodule
+//assign sum= in1 ^ in2 ^ Cin;
+//assign Cout = ((in1 ^ in2) & Cin) | (in1 & in2); 
+//endmodule
 
-module array_multiplier(input [1:0] a, b,
-    output [3:0] z
-    );
-  reg signed p[2][2];
-  wire[3:0] c;
-  wire [3:0] s;
-  genvar g;
+//module half_adder1(input a, b, output s0, c0);
+//  assign s0 = a ^ b;
+//  assign c0 = a & b;
+//endmodule
 
-  generate 
-    for(g=0;g<2;g=g+1)
-    begin
-      and(p[g][0],a[g],b[0]);
-      and(p[g][1],a[g],b[1]);
-    end
-  endgenerate
-  assign z[0] = p[0][0];
+//module array_multiplier(
+//    input [2:0] a, b,
+//    output [5:0] z
+//);
+//  wire p00,p01,p10,p11,p02,p21,p12,p20,p22;
+//  wire c0, c1;
 
-  half_adder h0(p[1][0],p[0][1],z[1],c[0]);
-  half_adder h1(p[1][1],c[0],z[2],c[1]);
-  endmodule
+//  // Partial products
+//  assign p00 = a[0] & b[0];
+//  assign p01 = a[0] & b[1];
+//  assign p10 = a[1] & b[0];
+//  assign p11 = a[1] & b[1];
+//  assign p02 = a[0] & b[2];
+//  assign p12 = a[1] & b[2];
+//  assign p21 = a[2] & b[1];
+//  assign p20 = a[2] & b[0];
+//  assign p22 = a[2] & b[2]; 
 
+//  assign z[0] = p00;
 
+//  // Using half adders
+//  half_adder1 h0(p10, p01, z[1], c0);
+//  half_adder1 h1(p11, c0, z[2], c1);
 
+//  assign z[3] = c1;
 
+//endmodule
 
+module array_multiplier(
+    input [3:0] a, b,
+    output [7:0] z
+);
 
+wire [7:0] m0, m1, m2, m3;
+wire [7:0] s1, s2, s3;
 
+// Partial products with proper alignment (shifts)
+assign m0 = (a & {4{b[0]}});           // no shift
+assign m1 = (a & {4{b[1]}}) << 1;      // shift by 1
+assign m2 = (a & {4{b[2]}}) << 2;      // shift by 2
+assign m3 = (a & {4{b[3]}}) << 3;      // shift by 3
 
+assign s1 = m0 + m1;
+assign s2 = s1 + m2;
+assign s3 = s2 + m3;
 
-
+assign z = s3;
 
 endmodule
--- Multiplier/Array Multiplier/tb_array_multiplier.v
+++ Multiplier/Array Multiplier/tb_array_multiplier.v
@@ -1,13 +1,18 @@
 module tb_array_multiplier();
-    reg [1:0] a,b;
-    wire [3:0] z;
+    reg [3:0] a,b;
+    wire [7:0] z;
 
     array_multiplier UUT (a,b,z);
 
     initial begin
         $monitor("A=%b, B=%b  --> Z=%b", a,b,z);
-        a=1;b=2;
+        a=5;b=6;
         #5 a=3;b=2;
-        #5 a=3;b=1;
+        #5 a=3;b=4;
+        #5 $finish();
     end
+    initial begin
+        $dumpvars(0);
+        $dumpfile("multiplier.vcd");
+        end
 endmodule
