02fc8cdbe150c3a44f67c8ab0429e9574087a5e0
MITT PARMAR <69619899+Mitt10@users.noreply.github.com>
2025-07-03 15:35:31 -0700

    Update Universal Shift Register.v - Parameterized shift reg code added

--- Register/Universal Shift Register/Universal Shift Register.v
+++ Register/Universal Shift Register/Universal Shift Register.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
-module universal_shift_reg(
+module universal_shift_reg #(WIDTH = 8)(
     input clk,rst,
     input[1:0] select,
-    input [3:0] p_din,
+    input [WIDTH-1:0] p_din,
     input s_left_din,
     input s_right_din,
-    output reg [3:0] p_dout,
+    output reg [WIDTH-1:0] p_dout,
     output s_left_dout,
     output s_right_dout
     );
@@ -18,15 +18,15 @@ begin
     else
     begin
         case(select)
-            2'b01: p_dout <= {s_right_din, p_dout[3:1]};
-            2'b10: p_dout <= {p_dout[2:0], s_left_din};
+            2'b01: p_dout <= {s_right_din, p_dout[WIDTH-1:1]};
+            2'b10: p_dout <= {p_dout[WIDTH-2:0], s_left_din};
             2'b11: p_dout <= p_din;
             default: p_dout <= p_dout;
         endcase
     end
 end
 
-assign s_left_dout = p_dout[3];
+assign s_left_dout = p_dout[WIDTH-1];
 assign s_right_dout = p_dout[0];
 
 endmodule
